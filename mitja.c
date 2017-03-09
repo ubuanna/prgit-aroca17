d6acc9a608071ca6742e8ba16d94a057a79a820b
daw2-Aguilar17 <https://github.com/daw2-Aguilar17/prgit-Aguilar17>
2017-03-09 20:38:08 +0100

    prgit-aroca17 en win 7

--- mitja.c
+++ mitja.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main() {
 	float suma=0.0;
 	float mitja=0.0;
 	float nou_valor=0.0;
 	int numero_repeticions=0;
-	char consulta='s';
+	char consulta='yes';
 	
-	while(consulta=='s') {
+	while(consulta=='yes') {
 		printf("Dòna la següent dada: ");
 		scanf("%f",&nou_valor);
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
-		printf("Vols continuar (s/n): ");
+		printf("Vols continuar (yes/n): ");
 		scanf("%s",&consulta); 
 	}
 	mitja=suma/numero_repeticions;
